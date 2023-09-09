bf3112fe07262b442d0d3fc36460fd719cd5c3fc
Raffa064 <raffaddtg@gmail.com>
2023-09-09 02:31:34 +0000

    Improved backend scalability: added update interval

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,12 +8,46 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 
+var currencyData = null
+const UPDATE_INTERVAL = process.env.UPDATE_INTERVAL || 1200000 // 20min
+var isUpdating = false
+var updatePromisse
+
+function updateCurrencyData() {
+  if (!isUpdating) {
+    isUpdating = true
+    updatePromisse = fetch('https://api.freecurrencyapi.com/v1/latest?base_currency=BRL&apikey=' + API_KEY)
+      .then(response => response.json())
+      .then(json => {
+        console.log('[Updated currency data]')
+        currencyData = {
+          data: json.data,
+          timestamp: Date.now()
+        }
+
+        isUpdating = false
+        updatePromisse = null
+      })
+
+    return updatePromisse
+  }
+
+  return updatePromisse
+}
+
 app.get('/currency', async (req, res) => {
-  fetch('https://api.freecurrencyapi.com/v1/latest?base_currency=BRL&apikey=' + API_KEY)
-    .then(response => response.json())
-    .then(json => {
-      res.send(json.data)
-    })
+  if (currencyData == null) {
+    updateCurrencyData().then(() => res.send(currencyData.data))
+    return
+  }
+
+  if (currencyData.timestamp < Date.now() - UPDATE_INTERVAL) {
+    updateCurrencyData().then(() => res.send(currencyData.data))
+    return
+  }
+
+  console.log('[Cached data sent ' + Date.now() + ' ]')
+  res.send(currencyData.data)
 })
 
 const PORT = 5000
